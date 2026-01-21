699242e26c2415d4b7170b08a3f03f253fed51dd
SamCut <samacut@outlook.com>
2026-01-20 19:01:15 -0700

    dsadas

--- startup.lua
+++ startup.lua
@@ -1,27 +1,23 @@
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
-local detector = peripheral.find("player_detector")
 
--- Use your specific file
-local audioFile = "/disk/AMERICA.dfpwm"
+local audioFile = "/AMERICA.dfpwm"
 
-local function playAmerica()
+local function playScatman()
     local decoder = dfpwm.make_decoder()
+    -- Open file with 'rb' (read binary)
     local file = io.open(audioFile, "rb")
     
     if not file then return print("File not found") end
 
-    term.clear()
-    term.setCursorPos(1,1)
-    print("OIL WELL AUDIO: PLAYING")
-
-    -- Exact audio loop from your working Scatman script
+    -- Using a smaller 8kb chunk instead of 16kb can stop screeching
     while true do
-        local chunk = file:read(8 * 1024)
+        local chunk = file:read(8 * 1024) 
         if not chunk then break end
         
         local buffer = decoder(chunk)
         
+        -- This ensures the speaker is actually ready before pushing more data
         while not speaker.playAudio(buffer) do
             os.pullEvent("speaker_audio_empty")
         end
@@ -32,6 +28,6 @@ end
 while true do
     os.pullEvent("redstone")
     if rs.getInput("back") then
-        playAmerica()
+        playScatman()
     end
 end
